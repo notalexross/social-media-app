c89fba13db0568aac2df28404ca536d0cac367a5
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2021-05-10 14:58:20 +0100

    feat: configure and initialize firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const { FieldValue } = firebase.firestore
+
+export { firebase, FieldValue }
