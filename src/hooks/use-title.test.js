a6f7787474de18e804e976d45dcf7674ed44a3aa
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2021-05-14 19:04:30 +0100

    test: add tests for useTitle hook

---
+++ src/hooks/use-title.test.js
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react'
+import useTitle from './use-title'
+
+function Component({ page }) {
+  useTitle(page)
+
+  return null
+}
+
+test('given no arguments, sets document title to value stored in environment', () => {
+  render(<Component />)
+
+  expect(document.title).toBe(process.env.REACT_APP_SITE_TITLE)
+})
+
+test('sets document title to value stored in environment with page name appended', () => {
+  render(<Component page="sign up" />)
+
+  expect(document.title).toBe(`sign up - ${process.env.REACT_APP_SITE_TITLE}`)
+})
