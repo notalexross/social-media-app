9091dc6c667545bb42e9e5821b1a0c897ec4271f
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2021-07-06 19:40:32 +0100

    feat: add sortBy and chunkArray utility functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -29,3 +29,33 @@ export function isValidSignInInputs({
 
   return hasPassword && hasValidEmail
 }
+
+export function sortBy<T extends Record<string, unknown>>(
+  array: T[],
+  property: keyof T,
+  direction?: 'desc' | 'asc'
+): T[] {
+  const newArray = [...array]
+  newArray.sort((a, b) => {
+    if (a[property] < b[property]) return -1
+    if (a[property] > b[property]) return 1
+
+    return 0
+  })
+
+  if (direction === 'desc') {
+    newArray.reverse()
+  }
+
+  return newArray
+}
+
+export function chunkArray<T>(array: T[], numPerChunk = 10): T[][] {
+  const tempArray = [...array]
+  const chunked: T[][] = []
+  while (tempArray.length) {
+    chunked.push(tempArray.splice(0, numPerChunk))
+  }
+
+  return chunked
+}
