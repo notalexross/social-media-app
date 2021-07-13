0f97c7e6f5bf4bb43e0e954ee668e8d0ec1dfded
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2021-07-13 13:54:04 +0100

    feat: extend usePosts() to accept a single post id

--- src/hooks/use-posts.ts
+++ src/hooks/use-posts.ts
@@ -3,13 +3,15 @@ import type { PostWithId, PostRepliesWithId } from '../services/firebase'
 import { getPosts } from '../services/firebase'
 
 export default function usePosts(
-  postIds: string[]
-): (PostWithId | PostRepliesWithId)[] {
-  const [posts, setPosts] = useState<(PostWithId | PostRepliesWithId)[]>([])
+  postIds: string[] | string
+): (PostWithId | PostRepliesWithId | undefined)[] | PostWithId | PostRepliesWithId | undefined {
+  const [posts, setPosts] = useState<(PostWithId | PostRepliesWithId | undefined)[]>([])
 
   useEffect(() => {
-    getPosts(postIds).then(setPosts).catch(console.error)
+    getPosts(Array.isArray(postIds) ? postIds : [postIds])
+      .then(setPosts)
+      .catch(console.error)
   }, [postIds])
 
-  return posts
+  return Array.isArray(postIds) ? posts : posts[0]
 }
