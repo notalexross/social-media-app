2c77912976c434a226e14f44239044190e254aa6
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2021-07-22 14:24:55 +0100

    feat: add timeline to dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,29 @@
-import { useTitle } from '../hooks'
+import { useContext } from 'react'
+import { UserContext } from '../context/user'
+import { TimelineContainer } from '../containers'
 import { Header } from '../components'
+import { useMultiUserPosts, usePostsLive, useTitle } from '../hooks'
 
 export default function DashboardPage(): JSX.Element {
   useTitle('Dashboard')
+  const { following } = useContext(UserContext)
+  const { posts, loadNextPage, isComplete, isLoadingPosts } = useMultiUserPosts(following, 2)
+  const postsLive = usePostsLive(posts)
 
-  return <Header />
+  return (
+    <>
+      <Header />
+      <main className="mx-4">
+        <div className="grid grid-cols-3 gap-4 mx-auto max-w-screen-lg">
+          <TimelineContainer
+            className="col-span-2"
+            posts={postsLive}
+            loadNextPage={loadNextPage}
+            isComplete={isComplete}
+            isLoadingPosts={isLoadingPosts}
+          />
+        </div>
+      </main>
+    </>
+  )
 }
