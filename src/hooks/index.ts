cbeaf458700b8b9e8766e832c01b20502c6b0bf5
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2021-08-19 23:00:57 +0100

    refactor: remove unused code

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,4 @@
 export { default as useAuthListener } from './use-auth-listener'
-export { default as useCachedUser } from './use-cached-user'
-export { default as useKeyDownListener } from './use-key-down-listener'
 export { default as useLockBody } from './use-lock-body'
 export { default as useMultiUserPosts } from './use-multi-user-posts'
 export { default as usePosts } from './use-posts'
