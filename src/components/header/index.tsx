b55c18a4971e4a575f36b3a60e82d40e1abb1f86
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2021-09-16 20:53:21 +0100

    refactor: replace div with header tag

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -11,7 +11,7 @@ export default function Header(): JSX.Element {
   const { user, avatar, username } = useContext(UserContext)
 
   return (
-    <div className="mb-2 py-4 border-b bg-white lg:mb-8">
+    <header className="mb-2 py-4 border-b bg-white lg:mb-8">
       <div className="mx-4">
         <div className="flex justify-between items-center mx-auto max-w-screen-lg">
           <h1 className="mt-1">
@@ -58,6 +58,6 @@ export default function Header(): JSX.Element {
           </div>
         </div>
       </div>
-    </div>
+    </header>
   )
 }
