23b61e39d13006e8cddccaea6a459676f1ca2b29
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2021-09-27 15:18:43 +0100

    refactor: remove unnecessary wrapper div

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -19,15 +19,14 @@ export default function DashboardPage({ timeline }: DashboardPageProps): JSX.Ele
   return (
     <main className="mx-2 lg:mx-4">
       <div className="grid grid-cols-3 gap-x-4 mx-auto max-w-screen-lg">
-        <div className="col-span-3 lg:col-span-2">
-          <TimelineContainer
-            posts={posts}
-            loadNextPage={loadNextPage}
-            isComplete={isComplete}
-            isLoadingPosts={isLoadingPosts}
-            error={error}
-          />
-        </div>
+        <TimelineContainer
+          className="col-span-3 lg:col-span-2"
+          posts={posts}
+          loadNextPage={loadNextPage}
+          isComplete={isComplete}
+          isLoadingPosts={isLoadingPosts}
+          error={error}
+        />
         <SidebarContainer className="self-start order-first col-span-3 mb-2 lg:sticky lg:top-4 lg:order-1 lg:col-span-1" />
       </div>
     </main>
