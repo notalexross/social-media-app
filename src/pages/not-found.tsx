e74435e5f59df250048b5276cd1bc7b5b7b70812
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2021-10-12 23:07:57 +0100

    fix: fix scrolling on not-found page

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,10 +1,12 @@
-import { useTitle } from '../hooks'
+import { useTitle, useWindowDimensions } from '../hooks'
 
 export default function NotFoundPage(): JSX.Element {
   useTitle('404')
+  const [, windowHeight] = useWindowDimensions()
+  const minHeight = windowHeight - 73
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
+    <div className="flex flex-col justify-center items-center" style={{ minHeight }}>
       <p className="text-center text-5xl">404</p>
       <p className="text-center text-5xl">Page Not Found</p>
     </div>
