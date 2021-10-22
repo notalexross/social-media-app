06f86a083febf39d2b3a789cf7db15e9495517ab
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2021-10-22 12:16:15 +0100

    fix: restore all overflows on useLockBody unmount

--- src/hooks/use-lock-body.ts
+++ src/hooks/use-lock-body.ts
@@ -4,6 +4,8 @@ export default function useLockBody(): void {
   useEffect(() => {
     const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth
     const initialOverflow = document.body.style.overflow
+    const initialOverflowX = document.body.style.overflowX
+    const initialOverflowY = document.body.style.overflowY
     const initialMarginRight = document.body.style.marginRight
     const bodyMarginRight = parseFloat(window.getComputedStyle(document.body).marginRight)
 
@@ -12,6 +14,8 @@ export default function useLockBody(): void {
 
     return () => {
       document.body.style.overflow = initialOverflow
+      document.body.style.overflowX = initialOverflowX
+      document.body.style.overflowY = initialOverflowY
       document.body.style.marginRight = initialMarginRight
     }
   }, [])
