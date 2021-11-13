3cefe0cbff9115d30e40a34d6d950085e73fe576
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2021-11-13 22:45:41 +0000

    feat: allow for multiple instances of hook

--- src/hooks/use-non-shifting-scrollbar.tsx
+++ src/hooks/use-non-shifting-scrollbar.tsx
@@ -1,37 +1,64 @@
-import { useEffect, useMemo } from 'react'
+import { useEffect } from 'react'
+
+let scrollbarWidth: number
+let initialOverflowX: string
+let initialWidth: string
+let initialPaddingRight: string
+let instances = 0
+
+function updateInitialStyles() {
+  initialOverflowX = document.body.style.overflowX
+  initialWidth = document.body.style.width
+  initialPaddingRight = document.body.style.paddingRight
+}
+
+function resetStyles() {
+  document.body.style.overflowX = initialOverflowX
+  document.body.style.width = initialWidth
+  document.body.style.paddingRight = initialPaddingRight
+}
+
+function updateStyles() {
+  const bodyPaddingRight = parseFloat(window.getComputedStyle(document.body).paddingRight)
+
+  document.body.style.overflowX = 'hidden'
+  document.body.style.width = '100vw'
+  document.body.style.paddingRight = `${bodyPaddingRight + scrollbarWidth}px`
+}
+
+function getScrollbarWidth() {
+  const measure = document.createElement('div')
+
+  measure.style.overflow = 'scroll'
+  measure.style.height = '0'
+  measure.style.visibility = 'hidden'
+
+  document.body.appendChild(measure)
+  scrollbarWidth = measure.offsetWidth - measure.clientWidth
+  document.body.removeChild(measure)
+}
 
 export default function useNonShiftingScrollbar(): number {
-  const scrollbarWidth = useMemo(() => {
-    const measure = document.createElement('div')
-    measure.style.overflow = 'scroll'
-    measure.style.height = '0'
-    measure.style.visibility = 'hidden'
-
-    document.body.appendChild(measure)
-
-    const width = measure.offsetWidth - measure.clientWidth
-
-    document.body.removeChild(measure)
-
-    return width
-  }, [])
+  if (!instances) {
+    getScrollbarWidth()
+  }
 
   useEffect(() => {
-    const initialOverflowX = document.body.style.overflowX
-    const initialWidth = document.body.style.width
-    const initialPaddingRight = document.body.style.paddingRight
-    const bodyPaddingRight = parseFloat(window.getComputedStyle(document.body).paddingRight)
+    instances += 1
 
-    document.body.style.overflowX = 'hidden'
-    document.body.style.width = '100vw'
-    document.body.style.paddingRight = `${bodyPaddingRight + scrollbarWidth}px`
+    if (instances === 1) {
+      updateInitialStyles()
+      updateStyles()
+    }
 
     return () => {
-      document.body.style.overflowX = initialOverflowX
-      document.body.style.width = initialWidth
-      document.body.style.paddingRight = initialPaddingRight
+      if (instances === 1) {
+        resetStyles()
+      }
+
+      instances -= 1
     }
-  }, [scrollbarWidth])
+  }, [])
 
   return scrollbarWidth
 }
